ff0beb2eba1dea9f0409d3403c9a9834655ab97b
TC030 <terencechanbb@gmail.com>
2026-07-06 18:29:49 +0800

    feat: implement abstract EnemyBase

---
+++ src/enemies/EnemyBase.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public abstract partial class EnemyBase : PathFollow2D
+{
+    [Export] public float Speed { get; protected set; } = 100f;
+    [Export] public float MaxHealth { get; protected set; } = 100f;
+    public float CurrentHealth { get; protected set; }
+
+    public override void _Ready()
+    {
+        base._Ready();
+        CurrentHealth = MaxHealth;
+    }
+    public virtual void TakeDamage(float amount)
+    {
+      CurrentHealth -= amount;
+      GD.Print($"{Name} took {amount} damage. Current health: {CurrentHealth}/{MaxHealth}");
+      if (CurrentHealth <= 0) Die();
+    }
+
+    protected virtual void Die()
+    {
+        GD.Print($"{Name} has died.");
+        QueueFree();
+    }
+}
